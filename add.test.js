f66facc96007f771118c69735eabe7248c5c189d
Vasant Saraswat <vasant.saraswat@gmail.com>
2021-11-27 17:24:06 +0530

    Assignment done

---
+++ add.test.js
@@ -0,0 +1,9 @@
+// const add = require('./add');
+
+// test('add 1 + 2 is equal to 3', () => {
+//   expect(add(1, 2)).toBe(3);
+// });
+
+// test('add 100 + 20 is equal to 120', () => {
+//   expect(add(100, 20)).toBe(120);
+// });
